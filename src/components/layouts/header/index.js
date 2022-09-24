ebac12ef4272efaf91605dd0cea9a8a5d88debaf
jagpratap <jagpratapchauhan@gmail.com>
2022-09-24 14:13:19 +0530

    header done

--- src/components/layouts/header/index.js
+++ src/components/layouts/header/index.js
@@ -1,7 +1,18 @@
 const Header = () => (
-  <div>
-    Header
-  </div>
+  <header className="header">
+    <div className="container">
+      <nav className="header-navbar">
+        <h1>audiophile</h1>
+        <ul className="nav-links">
+          <li><a href="/home">Home</a></li>
+          <li><a href="/headphones">Headphones</a></li>
+          <li><a href="/speakers">Speakers</a></li>
+          <li><a href="/earphones">Earphones</a></li>
+        </ul>
+        <img src="./assets/images/svg/shopping_cart.svg" alt="Shopping_Cart" />
+      </nav>
+    </div>
+  </header>
 );
 
 export default Header;
