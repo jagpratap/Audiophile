af518fcd79cc17ebbcdac37bc2bd6e2873ca93d9
jagpratap <jagpratapchauhan@gmail.com>
2022-09-24 23:16:57 +0530

    hero section setup with scss structure shuffle

--- src/components/app/home/index.jsx
+++ src/components/app/home/index.jsx
@@ -1,5 +1,22 @@
 const Home = () => (
-  <div>Home</div>
+  <section className="hero-section">
+    <div className="container">
+      <div className="content">
+        <div className="text">
+          <p className="subtitle">Product name</p>
+          <h1 className="title">XX99 Mark II Headphones</h1>
+          <p className="desc">
+            Experience natural, lifelike audio and exceptional build quality
+            made for the passionate music enthusiast.
+          </p>
+          <button type="button">See Product</button>
+        </div>
+        <div className="illustration">
+          <img src="./assets/images/jpg/hero-image.jpg" alt="hero-illustration" />
+        </div>
+      </div>
+    </div>
+  </section>
 );
 
 export default Home;
