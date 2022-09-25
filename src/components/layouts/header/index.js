b667fc75a0937f79611970dc4b33ab90ad7e8fd0
jagpratap <jagpratapchauhan@gmail.com>
2022-09-25 12:36:02 +0530

    Provided appropriate class names

--- src/components/layouts/header/index.js
+++ src/components/layouts/header/index.js
@@ -1,15 +1,15 @@
 const Header = () => (
   <header className="header">
     <div className="container">
-      <nav className="header-navbar">
-        <h1>audiophile</h1>
-        <ul className="nav-links">
+      <nav className="navbar">
+        <h1 className="navbar-brand" href="/">audiophile</h1>
+        <ul className="navbar_nav">
           <li><a href="/home">Home</a></li>
           <li><a href="/headphones">Headphones</a></li>
           <li><a href="/speakers">Speakers</a></li>
           <li><a href="/earphones">Earphones</a></li>
         </ul>
-        <img src="./assets/images/svg/shopping_cart.svg" alt="Shopping_Cart" />
+        <img className="navbar-cart" src="./assets/images/svg/shopping_cart.svg" alt="Shopping_Cart" />
       </nav>
     </div>
   </header>
