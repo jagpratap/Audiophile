52e8ec03dde229c72a5fdffde85599e7ab353424
jagpratap <jagpratapchauhan@gmail.com>
2022-09-25 23:57:14 +0530

    Header done with styling

---
+++ src/components/layouts/header/index.jsx
@@ -0,0 +1,18 @@
+const Header = () => (
+  <header className="header">
+    <div className="container">
+      <nav className="navbar">
+        <h1 className="navbar_brand" href="/">audiophile</h1>
+        <ul className="navbar_nav">
+          <li><a href="/home">Home</a></li>
+          <li><a href="/headphones">Headphones</a></li>
+          <li><a href="/speakers">Speakers</a></li>
+          <li><a href="/earphones">Earphones</a></li>
+        </ul>
+        <img className="navbar_cart" src="./assets/images/svg/shopping_cart.svg" alt="Shopping_Cart" />
+      </nav>
+    </div>
+  </header>
+);
+
+export default Header;
