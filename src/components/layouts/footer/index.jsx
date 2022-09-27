1e2e278c27c754fed688bf25fbaeba8368052191
jagpratap <jagpratapchauhan@gmail.com>
2022-09-27 11:52:32 +0530

    Footer section navbar

--- src/components/layouts/footer/index.jsx
+++ src/components/layouts/footer/index.jsx
@@ -1,7 +1,17 @@
 const Footer = () => (
-  <div>
-    Footer
-  </div>
+  <footer className="footer">
+    <div className="container">
+      <nav className="navbar">
+        <h1 className="navbar_brand" href="/">audiophile</h1>
+        <ul className="navbar_nav">
+          <li><a href="/home">Home</a></li>
+          <li><a href="/headphones">Headphones</a></li>
+          <li><a href="/speakers">Speakers</a></li>
+          <li><a href="/earphones">Earphones</a></li>
+        </ul>
+      </nav>
+    </div>
+  </footer>
 );
 
 export default Footer;
