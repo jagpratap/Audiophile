a173c9cd3d4beae37b485b61926bfe56521e42c1
jagpratap <jagpratapchauhan@gmail.com>
2022-09-29 19:54:01 +0530

    Header mobile first responsive layout with hamburger

--- src/components/layouts/header/index.jsx
+++ src/components/layouts/header/index.jsx
@@ -1,18 +1,44 @@
-const Header = () => (
-  <header className="header">
-    <div className="container">
-      <nav className="navbar">
-        <h5 className="navbar_brand" href="/">audiophile</h5>
-        <ul className="navbar_nav">
-          <li><a href="/home">Home</a></li>
-          <li><a href="/headphones">Headphones</a></li>
-          <li><a href="/speakers">Speakers</a></li>
-          <li><a href="/earphones">Earphones</a></li>
-        </ul>
-        <img className="navbar_cart" src="./assets/images/svg/shopping_cart.svg" alt="Shopping_Cart" />
-      </nav>
-    </div>
-  </header>
-);
+import { useState } from "react";
+
+const Header = () => {
+  const [toggle, setToggle] = useState(false);
+  return (
+    <header className="header">
+      <div className="container">
+        {/* Mobile Navbar */}
+        <nav className="navbar_mobile">
+          <div className="navbar_features">
+            <button
+              className="toggle_button"
+              type="button"
+              onClick={() => setToggle((prev) => !prev)}
+            >
+              <div className="hamburger" />
+            </button>
+            <h5 className="navbar_brand" href="/">audiophile</h5>
+            <img className="navbar_cart" src="./assets/images/svg/shopping_cart.svg" alt="Shopping_Cart" />
+          </div>
+          <ul className={toggle ? "navbar_nav--show" : "navbar_nav--hide"}>
+            <li><a href="/home">Home</a></li>
+            <li><a href="/headphones">Headphones</a></li>
+            <li><a href="/speakers">Speakers</a></li>
+            <li><a href="/earphones">Earphones</a></li>
+          </ul>
+        </nav>
+        {/* Laptop Navbar */}
+        <nav className="navbar_laptop">
+          <h5 className="navbar_brand" href="/">audiophile</h5>
+          <ul className="navbar_nav">
+            <li><a href="/home">Home</a></li>
+            <li><a href="/headphones">Headphones</a></li>
+            <li><a href="/speakers">Speakers</a></li>
+            <li><a href="/earphones">Earphones</a></li>
+          </ul>
+          <img className="navbar_cart" src="./assets/images/svg/shopping_cart.svg" alt="Shopping_Cart" />
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,6 @@
 import { Helmet } from "react-helmet";
 
-import Footer from "../components/layouts/footer";
+// import Footer from "../components/layouts/footer";
 import Header from "../components/layouts/header";
 
 const Layout = ({ children }) => (
@@ -8,7 +8,7 @@ const Layout = ({ children }) => (
     <Helmet titleTemplate="Audiophile | %s" />
     <Header />
     {children}
-    <Footer />
+    {/* <Footer /> */}
   </>
 );
 
